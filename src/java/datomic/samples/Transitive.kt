ffb7c0f5a6a529b9c316aeba86f6a8a9339518f6
Ian Rogers <ian@iprcom.com>
2020-07-21 18:36:52 +0100

    wip: added String-node shortest path

---
+++ src/java/datomic/samples/Transitive.kt
@@ -0,0 +1,192 @@
+package datomic.samples
+
+import datomic.Peer
+import datomic.Util
+import java.util.*
+
+/*
+A rewrite of parts of https://hashrocket.com/blog/posts/using-datomic-as-a-graph-database but in
+Kotlin and using the mbrainz dataset https://github.com/Datomic/mbrainz-sample
+ */
+object Transitive {
+    val rulessrc = """
+        [[(track-release ?t ?r) [?m :medium/tracks ?t] [?r :release/media ?m]]
+         [(track-info ?t ?track-name ?artist-name ?album ?year)
+          [?t :track/name ?track-name]
+          [?t :track/artists ?a]
+          [?a :artist/name ?artist-name]
+          (track-release ?t ?r)
+          [?r :release/name ?album]
+          [?r :release/year ?year]]
+         [(short-track ?a ?t ?len ?max)
+          [?t :track/artists ?a]
+          [?t :track/duration ?len]
+          [(< ?len ?max)]]
+         [(track-search ?q ?track)
+          [(fulltext ${'$'} :track/name ?q) [[?track ?tname]]]]
+         [(transitive-net-1 ?attr ?a1 ?a2)
+          [?x ?attr ?a1]
+          [?x ?attr ?a2]
+          [(!= ?a1 ?a2)]]
+         [(transitive-net-2 ?attr ?a1 ?a2) (transitive-net-1 ?attr ?a1 ?a2)]
+         [(transitive-net-2 ?attr ?a1 ?a2)
+          (transitive-net-1 ?attr ?a1 ?x)
+          (transitive-net-1 ?attr ?x ?a2)
+          [(!= ?a1 ?a2)]]
+         [(transitive-net-3 ?attr ?a1 ?a2) (transitive-net-1 ?attr ?a1 ?a2)]
+         [(transitive-net-3 ?attr ?a1 ?a2)
+          (transitive-net-2 ?attr ?a1 ?x)
+          (transitive-net-2 ?attr ?x ?a2)
+          [(!= ?a1 ?a2)]]
+         [(transitive-net-4 ?attr ?a1 ?a2) (transitive-net-1 ?attr ?a1 ?a2)]
+         [(transitive-net-4
+           ?attr ?a1 ?a2)
+          (transitive-net-3 ?attr ?a1 ?x)
+          (transitive-net-3 ?attr ?x ?a2)
+          [(!= ?a1 ?a2)]]
+         [(transitive-net-5 ?attr ?a1 ?a2) (transitive-net-1 ?attr ?a1 ?a2)]
+         [(transitive-net-5 ?attr ?a1 ?a2)
+          (transitive-net-4 ?attr ?a1 ?x)
+          (transitive-net-4 ?attr ?x ?a2)
+          [(!= ?a1 ?a2)]]
+         [(transitive-net-6 ?attr ?a1 ?a2) (transitive-net-1 ?attr ?a1 ?a2)]
+         [(transitive-net-6 ?attr ?a1 ?a2)
+          (transitive-net-5 ?attr ?a1 ?x)
+          (transitive-net-5 ?attr ?x ?a2)
+          [(!= ?a1 ?a2)]]
+         [(transitive-net-7 ?attr ?a1 ?a2) (transitive-net-1 ?attr ?a1 ?a2)]
+         [(transitive-net-7 ?attr ?a1 ?a2)
+          (transitive-net-6 ?attr ?a1 ?x)
+          (transitive-net-6 ?attr ?x ?a2)
+          [(!= ?a1 ?a2)]]
+         [(transitive-net-8 ?attr ?a1 ?a2) (transitive-net-1 ?attr ?a1 ?a2)]
+         [(transitive-net-8 ?attr ?a1 ?a2)
+          (transitive-net-7 ?attr ?a1 ?x)
+          (transitive-net-7 ?attr ?x ?a2)
+          [(!= ?a1 ?a2)]]
+         [(transitive-net-9 ?attr ?a1 ?a2) (transitive-net-1 ?attr ?a1 ?a2)]
+         [(transitive-net-9 ?attr ?a1 ?a2)
+          (transitive-net-8 ?attr ?a1 ?x)
+          (transitive-net-8 ?attr ?x ?a2)
+          [(!= ?a1 ?a2)]]
+         [(transitive-net-10 ?attr ?a1 ?a2) (transitive-net-1 ?attr ?a1 ?a2)]
+         [(transitive-net-10 ?attr ?a1 ?a2)
+          (transitive-net-9 ?attr ?a1 ?x)
+          (transitive-net-9 ?attr ?x ?a2)
+          [(!= ?a1 ?a2)]]
+         [(collab ?artist-name-1 ?artist-name-2)
+          [?a1 :artist/name ?artist-name-1]
+          (transitive-net-1 :track/artists ?a1 ?a2)
+          [?a2 :artist/name ?artist-name-2]]
+         [(collab-net-1 ?artist-name-1 ?artist-name-2)
+          (collab ?artist-name-1 ?artist-name-2)]
+         [(collab-net-2 ?artist-name-1 ?artist-name-2)
+          [?a1 :artist/name ?artist-name-1]
+          (transitive-net-2 :track/artists ?a1 ?a2)
+          [?a2 :artist/name ?artist-name-2]]
+         [(collab-net-3 ?artist-name-1 ?artist-name-2)
+          [?a1 :artist/name ?artist-name-1]
+          (transitive-net-3 :track/artists ?a1 ?a2)
+          [?a2 :artist/name ?artist-name-2]]
+         [(collab-net-4 ?artist-name-1 ?artist-name-2)
+          [?a1 :artist/name ?artist-name-1]
+          (transitive-net-4 :track/artists ?a1 ?a2)
+          [?a2 :artist/name ?artist-name-2]]
+         [(collab-net-5 ?artist-name-1 ?artist-name-2)
+          [?a1 :artist/name
+           ?artist-name-1]
+          (transitive-net-5 :track/artists ?a1 ?a2)
+          [?a2 :artist/name ?artist-name-2]]
+         [(collab-net-6 ?artist-name-1 ?artist-name-2)
+          [?a1 :artist/name ?artist-name-1]
+          (transitive-net-6 :track/artists ?a1 ?a2)
+          [?a2 :artist/name ?artist-name-2]]
+         [(collab-net-7 ?artist-name-1 ?artist-name-2)
+          [?a1 :artist/name ?artist-name-1]
+          (transitive-net-7 :track/artists ?a1 ?a2)
+          [?a2 :artist/name ?artist-name-2]]
+         [(collab-net-8 ?artist-name-1 ?artist-name-2)
+          [?a1 :artist/name ?artist-name-1]
+          (transitive-net-8 :track/artists ?a1 ?a2)
+          [?a2 :artist/name ?artist-name-2]]
+         [(collab-net-9 ?artist-name-1 ?artist-name-2)
+          [?a1 :artist/name ?artist-name-1]
+          (transitive-net-9 :track/artists ?a1 ?a2)
+          [?a2 :artist/name ?artist-name-2]]
+         [(collab-net-10 ?artist-name-1 ?artist-name-2)
+          [?a1 :artist/name ?artist-name-1]
+          (transitive-net-10 :track/artists ?a1 ?a2)
+          [?a2 :artist/name ?artist-name-2]]]
+    """.trimIndent()
+
+    fun shortestPath(start: String, end: String, neighbors: (String) -> List<String>) : List<String>? {
+        val seenitstart = mutableMapOf(start to listOf(start))
+        val seenitend = mutableMapOf(end to listOf(end));
+        val qs = LinkedList(listOf(start))
+        val qe = LinkedList(listOf(end))
+
+        fun cons(item: String, list:List<String>) : List<String> {
+            // not memory efficient as we don't have closure-like persistent data structures, but the lists won't be long
+            val coll = ArrayList<String>(list.size + 1)
+            coll.add(item)
+            coll.addAll(list)
+            return coll
+        }
+
+        fun takestep(
+                qworking: LinkedList<String>,
+                seenworking: MutableMap<String, List<String>>,
+                seenother: MutableMap<String, List<String>>
+        ) : List<String>? {
+            val item = qworking.pop()
+            if (seenother.contains(item)) {
+                return seenworking[item]!!.reversed().plus(seenother[item]!!.drop(1))
+            }
+            val itempath = seenworking[item]!!
+            for (next in neighbors(item)) {
+                if (! seenworking.containsKey(next)) {
+                    qworking.add(next)
+                    seenworking[next] = cons(next, itempath)
+                }
+            }
+
+            return null
+        }
+
+        while (qs.isNotEmpty() && qe.isNotEmpty()) {
+            val path = if (qs.size < qe.size) { // work on the side with the smallest branching first
+                takestep(qs, seenitstart, seenitend)
+            } else {
+                takestep(qe, seenitend, seenitstart)
+            }
+            if (path != null) {
+                return path
+            }
+        }
+
+        return null
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val conn = Peer.connect("datomic:dev://localhost:4334/mbrainz-1968-1973")
+        val db = conn.db()
+        val rules = Util.readAll(rulessrc.reader())[0]
+
+        println("Collaborated with...")
+        println(Peer.q(
+                "[:find ?aname ?aname2 :in $ % ?aname :where (collab ?aname ?aname2)]",
+                db, rules, "George Harrison"
+        ))
+        println("Collaborated with, up to 2 steps away...")
+        println(Peer.q(
+                "[:find ?aname ?aname2 :in $ % ?aname :where (collab-net-2 ?aname ?aname2)]",
+                db, rules, "George Harrison"
+        ))
+        println("Collaborated with, up to 3 steps away...")
+        println(Peer.q(
+                "[:find ?aname ?aname2 :in $ % ?aname :where (collab-net-3 ?aname ?aname2)]",
+                db, rules, "George Harrison"
+        ))
+    }
+}
---
+++ src/test/datomic/samples/TransitiveTest.kt
@@ -0,0 +1,23 @@
+package datomic.samples
+
+import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.matchers.collections.shouldBeIn
+import io.kotest.matchers.shouldBe
+
+class AnnotationSpecExample : AnnotationSpec() {
+    @Test
+    fun test1() {
+        val graph = mapOf( // undirected links
+                "a" to listOf("b"),
+                "b" to listOf("a", "c", "d"),
+                "c" to listOf("b", "e"),
+                "d" to listOf("b", "c", "e"),
+                "e" to listOf("c", "d", "f"),
+                "f" to listOf("e")
+        )
+
+        Transitive.shortestPath("a", "e") {graph[it]!!} shouldBeIn listOf(
+                listOf("a", "b", "c", "e"), listOf("a", "b", "d", "e")
+        )
+    }
+}
